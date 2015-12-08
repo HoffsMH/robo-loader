277002312baee6ddb03de643db5396721acc5d5a
HoffsMH <hecker.matthew@gmail.com>
2015-12-08 14:40:28 -0700

    made board file

--- test/board.js
+++ test/board.js
@@ -1,2 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+const Board = require('../lib/board');
+
+describe('Board object', function () {
+  it('should be an object', function () {
+    var board = new Board();
+    expect(board).to.be.a("Object");
+  });
+});
