17eb4f297d30979a17389c08fc43d8f82583caac
HoffsMH <hecker.matthew@gmail.com>
2015-12-08 23:26:18 -0700

    fixed spelling error where test reported that we were giving something called LaneHeight to the laneheight function
    as if!

--- test/get_lane_starts.js
+++ test/get_lane_starts.js
@@ -1,10 +1,10 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const getLaneHeight = require('../lib/util/get_lane_height');
+const getLaneStarts = require('../lib/util/get_lane_starts');
 
 describe('getLaneHeight', function () {
-  let laneHeight = getLaneHeight;
+  let laneStarts = getLaneStarts;
   context("when given laneCount and height", function () {
     let height = laneHeight(4, 100);
     it("returns lane height", function () {
