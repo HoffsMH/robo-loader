b25312a9d34ca8e75ce7b135d67ea1929a48bc78
HoffsMH <hecker.matthew@gmail.com>
2015-12-08 22:57:05 -0700

    added testfile for conveyor_area

---
+++ test/conveyor_area.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const ConveyorArea = require('../lib/conveyor_area');
+
+describe('ConveyorArea object', function () {
+  let conveyorArea = new ConveyorArea();
+
+  it('should be an object', function () {
+    expect(conveyorArea).to.be.a("Object");
+  });
+  describe("ConveyorArea state", function() {
+
+    it("should exist", function () {
+      expect(conveyorArea.state).to.not.eq(undefined);
+    });
+
+  });
+
+});
