43454858155411e39c5ce8934a04049a7618972f
HoffsMH <hecker.matthew@gmail.com>
2015-12-08 23:09:34 -0700

    cleaned up get_lane_height test file
    its interesting that our runner doesnt look in sub directories at this point
    should look into later

--- test/get_lane_height.js
+++ test/get_lane_height.js
@@ -4,11 +4,11 @@ const expect = chai.expect;
 const getLaneHeight = require('../lib/util/get_lane_height');
 
 describe('getLaneHeight', function () {
-  let laneHeight = getLaneHeight
+  let laneHeight = getLaneHeight;
   context("when given laneheight and count", function () {
-    let height = laneHeight(4, 100)
+    let height = laneHeight(4, 100);
     it("returns lane height", function () {
-      expect(height).to.eq(100)
+      expect(height).to.eq(25);
     });
   });
 
